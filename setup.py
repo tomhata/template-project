8415df973128ce92d24beffb5d86345cd172f23a
Tom Hata <tom@lookdeep.health>
2019-06-14 16:04:35 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML project template using cookie cutter methods.',
+    author='Tom Hata',
+    license='',
+)
